e9dbf014d7ec2b5b4e51ce19887dfea9847a5df6
Alessandro Moura <alessandromoura@MBPdeAlessandro.Home>
2026-01-04 14:43:03 +0000

    fix(staff): force update to fix build error

--- app/staff/[id]/page.tsx
+++ app/staff/[id]/page.tsx
@@ -21,6 +21,7 @@ export default function EditStaffPage() {
         workerNumber: "",
         name: "",
         contractType: "",
+        jobTitle: "", // Função
         group: "",
         area: "",
         workstation: "",
